cf787a4ef2151f7d2274d7c9041e7698471689ad
aisakova29 <99410542+aisakova29@users.noreply.github.com>
2022-07-27 20:53:01 +0300

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -16,9 +16,9 @@ for n in {1..5}; do
 
     if [ $? -eq 0 ]
     then
-       echo "Directory successfully created"
+       echo "Directory "$PTH""$name_directory"-"$i" successfully created"
     else
-       echo "Could not create directory" >&2
+       echo "Could not create "$PTH""$name_directory"-"$i" directory" >&2
     fi
 
 # создаем 1000 файлов с рандомными значениями в пяти ранее созданных директориях
@@ -53,3 +53,36 @@ mkdir "$PTH""$name_newdirectory"
 for dir in "$PTH""$name_directory"*; do
    cp -r "$dir" "$PTH""$name_newdirectory"
 done
+
+ if [ $? -eq 0 ]
+    then
+       echo "Copy of directories complete successfully"
+    else
+       echo "Could not copy directories" >&2
+    fi
+
+# сравниваем файлы в директориях и затем удаляем первые 5 созданных директорий
+
+for i in {1..5}; do
+    diff -r "$PTH""$name_directory"-"$i" "$PTH""$name_newdirectory"/"$name_directory"-"$i"
+    rm -R "$PTH""$name_directory"-"$i"
+    if [ $? -eq 0 ]
+    then
+       echo "Directory "$PTH""$name_directory"-"$i" delete successfully"
+    else
+       echo "Could not delete directory "$PTH""$name_directory"-"$i"" >&2
+    fi
+
+done
+
+# удаляем файлы с нечетными номерами в директориях с четными номерми
+
+find "$PTH""$name_newdirectory"/"$name_directory"-[0248] | egrep "file-([1,3,5,7,9]-|.[1,3,5,7,9]-|..[1,3,5,7,9]-)" | xargs rm -r
+
+ if [ $? -eq 0 ]
+    then
+       echo "Files delete successfully"
+    else
+       echo "Could not delete files" >&2
+  fi
+
