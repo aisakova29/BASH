cc84534b163ee271a117e8424ac99d52e874cb42
aisakova29 <99410542+aisakova29@users.noreply.github.com>
2022-07-27 16:00:15 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -23,3 +23,13 @@ for dir in "$PTH"dir*; do
    cp -r "$dir" "$PTH""$newdir"
 done
 
+
+for i in {1..5}; do
+    diff -r /home/ubuntu/dir-"$i" /home/ubuntu/new_dir/dir-"$i"
+    rm -R /home/ubuntu/dir-"$i"
+done
+
+find  /home/ubuntu/new_dir/dir-[0248]/ -type f -name 'file-[13579]-*' exec rm -f {} \;
+ls -al /home/ubuntu/new_dir/dir-[0248]/ | egrep "file-('[1,3,5,7,9]{1}'|*.[1,3,5,7,9])"
+
+
